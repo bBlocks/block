74aaf1e6201988c84713fba385f30443408d684e
Mikhail Vazhenin <mikhail.vazhenin@zs.com>
2018-02-27 17:14:38 -0600

    Example and story based on incremental mini game.

--- compose.js
+++ compose.js
@@ -17,9 +17,9 @@ function compose() {
 
 function inherit() {
 	var args = arguments;
-	if (args[0] == undefined) {args[0] = null;}
-	args[0] = Object.create(args[0]);
-	if (args.length == 1) {return args[0];}
+	if (args[0] == undefined) { args[0] = null; }
+	args[0] = Object.create(args[0], Object.getOwnPropertyDescriptors(args[0]));
+	if (args.length == 1) { return args[0]; }
 	return compose.apply(this, args);
 }
 
@@ -53,3 +53,40 @@ if (!Object.hasOwnProperty('getOwnPropertyDescriptors')) {
 		}
 	);
 }
+
+function assign(target, firstSource) {
+	if (target === undefined || target === null) {
+		throw new TypeError('Cannot convert first argument to object');
+	}
+
+	var to = Object(target);
+	for (var i = 1; i < arguments.length; i++) {
+		var nextSource = arguments[i];
+		if (nextSource === undefined || nextSource === null) {
+			continue;
+		}
+
+		var keysArray = Object.keys(Object(nextSource));
+		for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+			var nextKey = keysArray[nextIndex];
+			var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+			if (desc !== undefined && desc.enumerable) {
+				to[nextKey] = nextSource[nextKey];
+			}
+		}
+	}
+	return to;
+}
+
+function polyfill() {
+	if (!Object.assign) {
+		Object.defineProperty(Object, 'assign', {
+			enumerable: false,
+			configurable: true,
+			writable: true,
+			value: assign
+		});
+	}
+}
+
+polyfill();
