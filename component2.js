0fec0a2f3eced0af82fdec73f08ab31cfa2e67cb
webrealizer <webrealizer@gmail.com>
2018-03-04 22:58:07 -0600

    changes

--- component2.js
+++ component2.js
@@ -1,52 +1,66 @@
 'use strict';
+
 /**
  * Library building blocks 
  * @namespace
  * @version 0.1.2
  */
-var component = function(name) {
+var component = function(Prototype, name, tagName) {
 	if (!name) {
 		return;
 	}
-	if (!customElements.get(name)) {
-		var proto = {};
-		if (arguments.length >1 && window.bb && window.bb.compose) {
-			let args = Array.prototype.slice.call(arguments);
-			args[0] = proto;
-			bb.compose.apply(proto, args);
-		}
+	if (customElements.get(name)) {
+		throw 'Element "' + name + '" is already defined.';
+	}
+	if (Prototype != HTMLElement && (!tagName)) {
+		throw 'Tag name is required';
+	}
 
-		let ElementConstructor = Class({extends: HTMLElement, constructor: function () {
-			var self = this.super.apply(this, arguments);
-			if (typeof self.onCreate == 'function') {
-				if (!self._isCreated) { // Need to call onCreate only once.
-					self._isCreated = true;
-					self.onCreate();
-				}
+
+	let proto = {extends: Prototype || HTMLElement, constructor: function () {
+		var self = this.super.apply(this, arguments);
+		if (typeof self.onCreate == 'function') {
+			if (!self._isCreated) { // Need to call onCreate only once.
+				self._isCreated = true;
+				self.onCreate();
 			}
-			return self;
-		}});
-
-		if (proto) {
-			bb.compose(ElementConstructor.prototype, proto);
 		}
+		return self;
+	}};
+
+	if (arguments.length >3 && window.bb && window.bb.compose) {
+		let args = Array.prototype.slice.call(arguments, 2);
+		console.log(args);
+		args[0] = proto;
+		bb.compose.apply(null, args);
+	};
+
+	let ElementConstructor = Class(proto);
+
+
+	// Observed attributes		
+	if (ElementConstructor.prototype.getObservedAttributes) {
+		var descriptor = {
+			key: 'observedAttributes',
+			get: function get() {
+				return this.prototype.getObservedAttributes();
+			}
+		};		
+		descriptor.enumerable = descriptor.enumerable || false; 
+		descriptor.configurable = true; 
+		if ("value" in descriptor) descriptor.writable = true; 
+		Object.defineProperty(ElementConstructor, descriptor.key, descriptor); 	
+	};
+
+	let config;
+	if (tagName) {
+		config = {
+			extends: tagName
+		}
+	}
+
+	customElements.define(name, ElementConstructor, config);
 	
-		// Observed attributes		
-		if (ElementConstructor.prototype.getObservedAttributes) {
-			var descriptor = {
-				key: 'observedAttributes',
-				get: function get() {
-					return this.prototype.getObservedAttributes();
-				}
-			};		
-			descriptor.enumerable = descriptor.enumerable || false; 
-			descriptor.configurable = true; 
-			if ("value" in descriptor) descriptor.writable = true; 
-			Object.defineProperty(ElementConstructor, descriptor.key, descriptor); 	
-		};
-		
-		customElements.define(name, ElementConstructor);
-	}  
 	return customElements.get(name);
 };
 
